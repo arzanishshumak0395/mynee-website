e52d224cf8fcbf6fb196ca7e8455c8af14006400
Syed Arzanish <arzanishshumak0395@gmail.com>
2026-03-30 00:38:46 +0400

    changed mouse colour again

--- src/app/page.js
+++ src/app/page.js
@@ -75,7 +75,7 @@ export default function Home() {
           left: `${mousePosition.x}px`,
           top: `${mousePosition.y}px`,
           transform: "translate(-50%, -50%)",
-          background: "radial-gradient(circle, rgba(95, 101, 94, 0.45) 0%, transparent 70%)"
+          background: "radial-gradient(circle, rgba(50, 223, 177, 0.75) 0%, transparent 70%)"
         }}
       />
 
